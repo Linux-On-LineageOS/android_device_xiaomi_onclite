cbd496bf4320502e7c855a47d769c2639819e3d3
ArchieMeng <archiemeng@protonmail.com>
2025-06-05 20:12:52 +0800

    Include Lindroid vendor

--- device.mk
+++ device.mk
@@ -425,3 +425,4 @@ PRODUCT_PACKAGES += \
 
 # Get non-open-source specific aspects
 $(call inherit-product, vendor/xiaomi/onclite/onclite-vendor.mk)
+$(call inherit-product, vendor/lindroid/lindroid.mk)
